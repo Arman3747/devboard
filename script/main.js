db962b20c98add20ad40e35a765f49d202a8aa25
Arman3747 <dailyarman7@gmail.com>
2025-03-02 15:21:33 +1100

    Blogs added

--- script/main.js
+++ script/main.js
@@ -1,1 +1,16 @@
 console.log("Hello world!")
+
+// document.getElementsByClassName('btn-completed')
+//     .addEventListener('click', function(){
+//         alert("Board updated successfully!");
+
+//         const taskRemaining = document.getElementById('task-remaining');
+        
+
+// })
+
+
+
+
+
+
